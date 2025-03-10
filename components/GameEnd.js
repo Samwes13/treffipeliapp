7913d31c62109f3330574df9a602594fddfa6164
Samwes13 <sampo.westerholm@gmail.com>
2025-03-10 21:51:38 +0200

    Changes

--- components/GameEnd.js
+++ components/GameEnd.js
@@ -4,6 +4,7 @@ import { ref, update, remove } from 'firebase/database';
 import { database } from '../firebaseConfig';
 import styles from '../styles';
 import { LinearGradient } from 'expo-linear-gradient';
+import AdSenseBanner from './AdSenseBanner'; // Tuo AdSense-komponentti
 
 export default function GameEnd({ route, navigation }) {
   const { gamepin, username } = route.params || {};
@@ -65,17 +66,7 @@ export default function GameEnd({ route, navigation }) {
       </TouchableOpacity>
 
       {/* AdSense-mainos */}
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7869485729301293"
-        crossorigin="anonymous"></script>
-      <ins className="adsbygoogle"
-        style={{ display: 'block' }}
-        data-ad-client="ca-pub-7869485729301293"
-        data-ad-slot="1234567890"
-        data-ad-format="auto"
-        data-full-width-responsive="true"></ins>
-      <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-      </script>
+      <AdSenseBanner />
     </View>
   );
 }
